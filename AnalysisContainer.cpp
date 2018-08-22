56db908411b0dee3d3cc9df8b9d81cb041593b22
TwITe <dm.blink19@gmail.com>
2018-08-22 18:26:15 +0300

    add AnalysisContainer class's implementation file

---
+++ AnalysisContainer.cpp
@@ -0,0 +1,23 @@
+#include "AnalysisContainer.h"
+
+Token AnalysisContainer::GetNextToken() {
+    auto token = tokens[currentTokenNum];
+    currentTokenNum++;
+    return token;
+}
+
+void AnalysisContainer::ReturnToken() {
+    currentTokenNum--;
+}
+
+bool AnalysisContainer::IsIdExist(std::string identifierName) {
+    return symbol_table.find(identifierName) != symbol_table.end();
+}
+
+void AnalysisContainer::AddNewIdentifier(std::string identifierName, const Identifier& identifier) {
+    symbol_table.emplace(identifierName, identifier);
+}
+
+void AnalysisContainer::AddNewToken(Token token) {
+    tokens.emplace_back(token);
+}
