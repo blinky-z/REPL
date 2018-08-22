36d635b7ef8f4b027c98c54f843eab72fc48c419
TwITe <dm.blink19@gmail.com>
2018-08-22 18:18:38 +0300

    exclude numbers and ids parsing code to functions

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
 #include "Lexer.h"
 
-void Lexer::tokenize(char* src, vector<Token>& tokens, unordered_map<string, Identifier>& symbol_table) {
+AnalysisContainer Lexer::tokenize(char* src) {
     while (*src != EOF) {
         Token token;
 
@@ -13,90 +14,108 @@ void Lexer::tokenize(char* src, vector<Token>& tokens, unordered_map<string, Ide
         }
 
         if ((*src >= 'a' && *src <= 'z') || (*src >= 'A' && *src <= 'Z') || (*src == '_')) {
-            string id_name;
-            id_name += *src;
-
-            while ((*(src + 1) >= 'a' && *(src + 1) <= 'z') || (*(src + 1) >= 'A' && *(src + 1) <= 'Z') ||
-                   (*(src + 1) >= '0' && *(src + 1) <= '9') || (*(src + 1) == '_')) {
-                src++;
-                id_name += *src;
-            }
-
-            // объявление переменной
-            if (id_name == "var") {
-                token.type = DeclareId;
-                token.value = "var";
-            } else {
-                token.type = Id;
-                token.value = id_name;
-
-                // если такого Identifier в таблице символов ещё нет, внести данные о нем
-                if (symbol_table.find(id_name) == symbol_table.end()) {
-                    symbol_table.emplace(id_name, Identifier{});
-                }
-            }
+            token = tokenizeIdentifier(&src);
         } else if (*src >= '0' && *src <= '9') {
-            int token_val = *src - '0';
-
-            while (*(src + 1) >= '0' && *(src + 1) <= '9') {
-                src++;
-                token_val = token_val * 10 + *src - '0';
-            }
-
-            token.type = Num;
-            token.value = to_string(token_val);
+            token = tokenizeNumber(&src);
         } else if (*src == '=') {
-            token.type = Assign;
+            token.type = data.Assign;
             token.value = "=";
         } else if (*src == '+') {
             if (*(src + 1) == '+') {
                 src++;
 
-                token.type = Inc;
+                token.type = data.Inc;
                 token.value = "++";
             } else {
-                token.type = Add;
+                token.type = data.Add;
                 token.value = "+";
             }
         } else if (*src == '-') {
             if (*(src + 1) == '-') {
                 src++;
 
-                token.type = Dec;
+                token.type = data.Dec;
                 token.value = "--";
             } else {
-                token.type = Sub;
+                token.type = data.Sub;
                 token.value = "-";
             }
         } else if (*src == '*') {
-            token.type = Mul;
+            token.type = data.Mul;
             token.value = "*";
         } else if (*src == '/') {
-            token.type = Div;
+            token.type = data.Div;
             token.value = "/";
         } else if (*src == '%') {
-            token.type = Mod;
+            token.type = data.Mod;
             token.value = "%";
         } else if (*src == '(') {
-            token.type = ROUND_BRACKET_START;
+            token.type = data.ROUND_BRACKET_START;
             token.value = "(";
         } else if (*src == ')') {
-            token.type = ROUND_BRACKET_END;
+            token.type = data.ROUND_BRACKET_END;
             token.value = ")";
         } else if (*src == '[') {
-            token.type = SQUARE_BRACKET_START;
+            token.type = data.SQUARE_BRACKET_START;
             token.value = "[";
         } else if (*src == ']') {
-            token.type = SQUARE_BRACKET_END;
+            token.type = data.SQUARE_BRACKET_END;
             token.value = "]";
         } else if (*src == ';') {
-            token.type = SEMICOLON;
+            token.type = data.SEMICOLON;
             token.value = ";";
         }
 
-        tokens.emplace_back(token);
+        data.AddNewToken(token);
         src++;
     }
 
-    tokens.emplace_back(Token{eof, "EOF"});
+    data.AddNewToken({data.eof, "EOF"});
+
+    return data;
+}
+
+Token Lexer::tokenizeIdentifier(char** src) {
+    Token token;
+
+    string id_name;
+    id_name += *src;
+
+    while ((**(src + 1) >= 'a' && **(src + 1) <= 'z') || (**(src + 1) >= 'A' && **(src + 1) <= 'Z') ||
+           (**(src + 1) >= '0' && **(src + 1) <= '9') || (**(src + 1) == '_')) {
+        src++;
+        id_name += *src;
+    }
+
+    // объявление переменной
+    if (id_name == "var") {
+        token.type = data.DeclareId;
+        token.value = "var";
+    } else {
+        token.type = data.Id;
+        token.value = id_name;
+
+        // если такого Identifier в таблице символов ещё нет, внести данные о нем
+        if (!data.IsIdExist(id_name)) {
+            data.AddNewIdentifier(id_name, Identifier{});
+        }
+    }
+
+    return token;
+}
+
+Token Lexer::tokenizeNumber(char** src) {
+    Token token;
+
+    int token_val = **src - '0';
+
+    while (*(*src + 1) >= '0' && *(*src + 1) <= '9') {
+        (*src)++;
+        token_val = token_val * 10 + **src - '0';
+    }
+
+    token.type = data.Num;
+    token.value = to_string(token_val);
+
+    return token;
 }
--- Lexer.h
+++ Lexer.h
@@ -3,6 +3,7 @@
 
 #include "Token.h"
 #include "Identifier.h"
+#include "AnalysisContainer.h"
 #include <vector>
 #include <string>
 #include <unordered_map>
@@ -11,32 +12,15 @@ using namespace std;
 
 class Lexer {
 private:
-    enum types {
-        eof,
-        Num,
-        String,
-        Keyword,
-        DeclareId,
-        DeclareFunc,
-        Id,
-        ROUND_BRACKET_START,
-        ROUND_BRACKET_END,
-        SQUARE_BRACKET_START,
-        SQUARE_BRACKET_END,
-        SEMICOLON,
-        Assign,
-        Add,
-        Sub,
-        Mul,
-        Div,
-        Mod,
-        Inc,
-        Dec
-    };
-
     int line = 1;
+
+    AnalysisContainer data;
+
+    Token tokenizeIdentifier(char** src);
+
+    Token tokenizeNumber(char** src);
 public:
-    void tokenize(char* src, vector<Token>& tokens, unordered_map<string, Identifier>& symbol_table);
+    AnalysisContainer tokenize(char* src);
 };
 
 #endif //BASHCOMPILER_LEXER_H
