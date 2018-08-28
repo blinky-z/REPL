06327110a6f5b697be937da8a63b7940fa9b93a6
TwITe <dm.blink19@gmail.com>
2018-08-28 20:27:22 +0300

    extend ASTNode struct to more types (Numbers, Ids)

--- ASTNode.h
+++ ASTNode.h
@@ -1,34 +1,52 @@
 #ifndef REPL_ASTNODE_H
 #define REPL_ASTNODE_H
 
+#include <string>
+
 enum ASTNodeType {
     Undefined,
     Empty,
+    Id,
+    NumberValue,
     OperatorPlus,
     OperatorMinus,
     OperatorMul,
     OperatorDiv,
-    NumberValue
 };
 
 struct ASTNode {
-    ASTNodeType Type;
-    double Value;
-    ASTNode* Left;
-    ASTNode* Right;
+    ASTNodeType type;
+};
 
-    ASTNode() {
-        Type = Undefined;
-        Value = 0;
-        Left = nullptr;
-        Right = nullptr;
+struct BinOpNode : ASTNode {
+    ASTNode* left;
+    ASTNode* right;
+
+    BinOpNode() {
+        left = nullptr;
+        right = nullptr;
     }
 
-    ~ASTNode() {
-        delete Left;
-        delete Right;
+    ~BinOpNode() {
+        delete left;
+        delete right;
     }
 };
 
+struct NumberNode : ASTNode {
+    double value;
+
+    NumberNode() {
+        type = NumberValue;
+    }
+};
+
+struct IdentifierNode : ASTNode {
+    std::string name;
+
+    IdentifierNode() {
+        type = Id;
+    }
+};
 
 #endif //REPL_ASTNODE_H
