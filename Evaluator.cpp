b15552b9061a48e4503f09022e1444ee0b4ee67d
TwITe <dm.blink19@gmail.com>
2018-08-29 12:05:13 +0300

    Add else statement, replace if construction with switch
    1. Else statement throws "Invalid AST" runtime_error
    2. If construction that handles Binary Operation type determination was replaced with similar switch construction but adding default error case

--- Evaluator.cpp
+++ Evaluator.cpp
@@ -4,20 +4,25 @@ double Evaluator::EvaluateSubtree(ASTNode* subtree) {
     if (subtree->type == NumberValue) {
         NumberNode* node = static_cast<NumberNode*>(subtree);
         return node->value;
-    } else {
+    } else if (subtree->type == BinOp) {
         BinOpNode* node = static_cast<BinOpNode*>(subtree);
         double leftValue = EvaluateSubtree(node->left);
         double rightValue = EvaluateSubtree(node->right);
 
-        if (node->binOpType == OperatorPlus) {
-            return leftValue + rightValue;
-        } else if (node->binOpType == OperatorMinus) {
-            return leftValue - rightValue;
-        } else if (node->binOpType == OperatorMul) {
-            return leftValue * rightValue;
-        } else {
-            return leftValue / rightValue;
+        switch (node->binOpType) {
+            case OperatorPlus:
+                return leftValue + rightValue;
+            case OperatorMinus:
+                return leftValue - rightValue;
+            case OperatorMul:
+                return leftValue * rightValue;
+            case OperatorDiv:
+                return leftValue / rightValue;
+            default:
+                throw std::runtime_error("Unknown binary operation");
         }
+    } else {
+        throw std::runtime_error("Invalid AST");
     }
 }
 
