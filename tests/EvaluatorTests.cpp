6d2a0b54755396f8e1bf6943e4dbf76298b7b55a
TwITe <dm.blink19@gmail.com>
2018-08-29 23:13:32 +0300

    Add new Evaluator tests
    Add new tests checks using of unary minus

--- tests/EvaluatorTests.cpp
+++ tests/EvaluatorTests.cpp
@@ -424,3 +424,66 @@ TEST_CASE("Assign variable to the other variable", "[Evaluator]") {
 
     REQUIRE(result == properResult);
 }
+
+TEST_CASE("Make variable negative by multiplication on -1 using unary minus", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    double a = 200 * -1;
+
+    std::string expr1 = "var a = 200";
+    std::string expr2 = "a = a * -1";
+    std::string expr3 = "a";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+    expr3.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+    const TokenContainer& tokensExpr3 = EvaluatorTestsLexer.tokenize(expr3);
+
+    // declare and assign var evaluate
+    EvaluatorTestsEvaluator.Evaluate(EvaluatorTestsParser.parse(tokensExpr1));
+
+    // multiplication on -1
+    EvaluatorTestsEvaluator.Evaluate(EvaluatorTestsParser.parse(tokensExpr2));
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokensExpr3);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = std::to_string(a);
+
+    REQUIRE(result == properResult);
+}
+
+TEST_CASE("Make variable negative by assigning on other negative variable using unary minus", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    double a = 200;
+    double b = a * -1;
+
+    std::string expr1 = "var a = 200";
+    std::string expr2 = "var b = -a";
+    std::string expr3 = "b";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+    expr3.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+    const TokenContainer& tokensExpr3 = EvaluatorTestsLexer.tokenize(expr3);
+
+    // declare and assign var a evaluate
+    EvaluatorTestsEvaluator.Evaluate(EvaluatorTestsParser.parse(tokensExpr1));
+
+    // declare and assign var b evaluate
+    EvaluatorTestsEvaluator.Evaluate(EvaluatorTestsParser.parse(tokensExpr2));
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokensExpr3);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = std::to_string(b);
+
+    REQUIRE(result == properResult);
+}
