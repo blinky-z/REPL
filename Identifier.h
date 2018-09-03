b83f44e07710b703908c4425d119c26e15540de7
TwITe <dm.blink19@gmail.com>
2018-09-03 22:15:13 +0300

    Add 'BoolValue' field of type bool to Identifier

--- Identifier.h
+++ Identifier.h
@@ -7,6 +7,7 @@ struct Identifier {
     int Type; // тип контента (string, int в bash)
     std::string StringValue; // в bash есть только int и string
     double NumValue;
+    bool BoolValue;
 };
 
 #endif //BASHCOMPILER_IDENTIFIER_H
