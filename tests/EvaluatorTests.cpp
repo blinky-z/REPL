775e726de083b67954a437eda45ed6aae951c60f
TwITe <dm.blink19@gmail.com>
2018-09-04 23:26:42 +0300

    Add new Evaluator tests (using of bool values covering)

--- tests/EvaluatorTests.cpp
+++ tests/EvaluatorTests.cpp
@@ -10,6 +10,10 @@
 Lexer EvaluatorTestsLexer;
 Parser EvaluatorTestsParser;
 
+std::string getBoolValueStringRepresentation(bool value) {
+    return value ? "true" : "false";
+}
+
 TEST_CASE("Addition Evaluation", "[Evaluator][Math operations evaluating]") {
     Evaluator EvaluatorTestsEvaluator;
 
@@ -696,4 +700,463 @@ TEST_CASE("Declare & assign negative float point number value to variable", "[Ev
 
     delete root1;
     delete root2;
+}
+
+TEST_CASE("Single bool value evaluation: true", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "true";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(true);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Single bool value evaluation: false", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "false";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(false);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical OR with different bool values", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "false || true";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(false || true);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical OR with the same false bool values",
+          "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "false || false";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(false || false);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical OR with the same true bool values", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "true || true";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(true || true);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical AND with different bool values", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "false && true";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(false && true);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical AND with the same false bool values", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "false && false";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(false && false);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical AND with the same true bool values", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "true && true";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(true && true);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical AND and OR [1]", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "false && true || false";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(false && true || false);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical AND and OR [2]", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "false && true || true";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(false && true || true);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical AND and OR changing the order of operations [1]",
+          "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "false && (true || false)";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation(false && (true || false));
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Bool expression evaluation: using of Logical AND and OR changing the order of operations [2]",
+          "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr = "(true || false) && false";
+
+    std::string parseExpr = expr;
+    parseExpr.push_back(EOF);
+
+    const TokenContainer& tokens = EvaluatorTestsLexer.tokenize(parseExpr);
+
+    ASTNode* root = EvaluatorTestsParser.parse(tokens);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root);
+    std::string properResult = getBoolValueStringRepresentation((true || false) && false);
+
+    REQUIRE(result == properResult);
+
+    delete root;
+}
+
+TEST_CASE("Declare & assign variable of type bool in single expr", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    bool a = true;
+
+    std::string expr1 = "var a = true";
+    std::string expr2 = "a";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+
+    // declare and assign var evaluate
+    ASTNode* root1 = EvaluatorTestsParser.parse(tokensExpr1);
+    EvaluatorTestsEvaluator.Evaluate(root1);
+
+    ASTNode* root2 = EvaluatorTestsParser.parse(tokensExpr2);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root2);
+    std::string properResult = getBoolValueStringRepresentation(a);
+
+    REQUIRE(result == properResult);
+
+    delete root1;
+    delete root2;
+}
+
+TEST_CASE("Declare & assign variable of type bool in separate expr", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    bool a = false;
+
+    std::string expr1 = "var a";
+    std::string expr2 = "a = false";
+    std::string expr3 = "a";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+    expr3.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+    const TokenContainer& tokensExpr3 = EvaluatorTestsLexer.tokenize(expr3);
+
+    // declare var evaluate
+    ASTNode* root1 = EvaluatorTestsParser.parse(tokensExpr1);
+    EvaluatorTestsEvaluator.Evaluate(root1);
+
+    // assign var evaluate
+    ASTNode* root2 = EvaluatorTestsParser.parse(tokensExpr2);
+    EvaluatorTestsEvaluator.Evaluate(root2);
+
+    ASTNode* root3 = EvaluatorTestsParser.parse(tokensExpr3);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root3);
+    std::string properResult = getBoolValueStringRepresentation(a);
+
+    REQUIRE(result == properResult);
+
+    delete root1;
+    delete root2;
+    delete root3;
+}
+
+TEST_CASE("Assign variable of type bool to the other variable of type bool", "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    bool a = true;
+    bool b = a;
+
+    std::string expr1 = "var a = true";
+    std::string expr2 = "var b = a";
+    std::string expr3 = "b";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+    expr3.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+    const TokenContainer& tokensExpr3 = EvaluatorTestsLexer.tokenize(expr3);
+
+    // declare var a evaluate
+    ASTNode* root1 = EvaluatorTestsParser.parse(tokensExpr1);
+    EvaluatorTestsEvaluator.Evaluate(root1);
+
+    // declare var b and assign to var a evaluate
+    ASTNode* root2 = EvaluatorTestsParser.parse(tokensExpr2);
+    EvaluatorTestsEvaluator.Evaluate(root2);
+
+    ASTNode* root3 = EvaluatorTestsParser.parse(tokensExpr3);
+
+    std::string result = EvaluatorTestsEvaluator.Evaluate(root3);
+    std::string properResult = getBoolValueStringRepresentation(b);
+
+    REQUIRE(result == properResult);
+
+    delete root1;
+    delete root2;
+    delete root3;
+}
+
+TEST_CASE("Get exception on assign variable of type bool to the value of INCOMPATIBLE type: int to bool",
+          "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr1 = "var b = 5";
+    std::string expr2 = "b = false";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+
+    ASTNode* root1 = EvaluatorTestsParser.parse(tokensExpr1);
+    EvaluatorTestsEvaluator.Evaluate(root1);
+
+    ASTNode* root2 = EvaluatorTestsParser.parse(tokensExpr2);
+
+    REQUIRE_THROWS(EvaluatorTestsEvaluator.Evaluate(root2));
+
+    delete root1;
+    delete root2;
+}
+
+TEST_CASE("Get exception on assign variable of type bool to the value of INCOMPATIBLE type: bool to int",
+          "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr1 = "var b = true";
+    std::string expr2 = "b = 5";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+
+    ASTNode* root1 = EvaluatorTestsParser.parse(tokensExpr1);
+    EvaluatorTestsEvaluator.Evaluate(root1);
+
+    ASTNode* root2 = EvaluatorTestsParser.parse(tokensExpr2);
+
+    REQUIRE_THROWS(EvaluatorTestsEvaluator.Evaluate(root2));
+
+    delete root1;
+    delete root2;
+}
+
+TEST_CASE("Get exception on assign variable of type bool to the other variable of INCOMPATIBLE type: int to bool",
+          "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr1 = "var a = true";
+    std::string expr2 = "var b = 5";
+    std::string expr3 = "b = a";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+    expr3.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+    const TokenContainer& tokensExpr3 = EvaluatorTestsLexer.tokenize(expr3);
+
+    // declare var a evaluate
+    ASTNode* root1 = EvaluatorTestsParser.parse(tokensExpr1);
+    EvaluatorTestsEvaluator.Evaluate(root1);
+
+    // declare var b and assign to var a evaluate
+    ASTNode* root2 = EvaluatorTestsParser.parse(tokensExpr2);
+    EvaluatorTestsEvaluator.Evaluate(root2);
+
+    ASTNode* root3 = EvaluatorTestsParser.parse(tokensExpr3);
+    REQUIRE_THROWS(EvaluatorTestsEvaluator.Evaluate(root3));
+
+    delete root1;
+    delete root2;
+    delete root3;
+}
+
+TEST_CASE("Get exception on assign variable of type bool to the other variable of INCOMPATIBLE type: bool to int",
+          "[Evaluator]") {
+    Evaluator EvaluatorTestsEvaluator;
+
+    std::string expr1 = "var a = 5";
+    std::string expr2 = "var b = false";
+    std::string expr3 = "b = a";
+
+    expr1.push_back(EOF);
+    expr2.push_back(EOF);
+    expr3.push_back(EOF);
+
+    const TokenContainer& tokensExpr1 = EvaluatorTestsLexer.tokenize(expr1);
+    const TokenContainer& tokensExpr2 = EvaluatorTestsLexer.tokenize(expr2);
+    const TokenContainer& tokensExpr3 = EvaluatorTestsLexer.tokenize(expr3);
+
+    // declare var a evaluate
+    ASTNode* root1 = EvaluatorTestsParser.parse(tokensExpr1);
+    EvaluatorTestsEvaluator.Evaluate(root1);
+
+    // declare var b and assign to var a evaluate
+    ASTNode* root2 = EvaluatorTestsParser.parse(tokensExpr2);
+    EvaluatorTestsEvaluator.Evaluate(root2);
+
+    ASTNode* root3 = EvaluatorTestsParser.parse(tokensExpr3);
+    REQUIRE_THROWS(EvaluatorTestsEvaluator.Evaluate(root3));
+
+    delete root1;
+    delete root2;
+    delete root3;
 }
