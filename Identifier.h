28d4547419f1ddc94136db2752e7a59ad704c9b4
TwITe <dm.blink19@gmail.com>
2018-09-04 23:02:50 +0300

    Add id type enum values to Identifier
    1. Add IdentifierValueType struct that contains the following enum types: Number, Bool, Undefined
    2. Add string representation of above enum types

--- Identifier.h
+++ Identifier.h
@@ -2,12 +2,35 @@
 #define BASHCOMPILER_IDENTIFIER_H
 
 #include <string>
+#include <unordered_map>
+
+struct IdentifierValueType {
+    enum ValueType {
+        Number,
+        Bool,
+        Undefined
+    };
+};
+
+struct IdentifierTypeStringNames {
+    std::unordered_map<int, std::string> idTypeStringNames;
+
+    IdentifierTypeStringNames() {
+        idTypeStringNames[IdentifierValueType::Number] = "Number";
+        idTypeStringNames[IdentifierValueType::Bool] = "Bool";
+        idTypeStringNames[IdentifierValueType::Undefined] = "Undefined";
+    }
+};
 
 struct Identifier {
     int Type; // тип контента (string, int в bash)
     std::string StringValue; // в bash есть только int и string
     double NumValue;
     bool BoolValue;
+
+    Identifier() {
+        Type = IdentifierValueType::Undefined;
+    }
 };
 
 #endif //BASHCOMPILER_IDENTIFIER_H
