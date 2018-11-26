ba9c29744f69f190933a5feb404c38d5d97572ba
TwITe <dm.blink19@gmail.com>
2018-11-26 23:02:27 +0300

    Add all operations code generating support to Bash Generator

--- BashGenerator.h
+++ BashGenerator.h
@@ -3,25 +3,75 @@
 
 #include "ASTNode.h"
 #include <string>
+#include <unordered_map>
 #include <cmath>
+#include <algorithm>
+#include "sole/sole.hpp"
 
 class BashGenerator {
 private:
+    struct Scope {
+        Scope* outer;
+        std::unordered_map<std::string, std::string> uuid;
+
+        Scope(Scope* outerScope) {
+            outer = outerScope;
+        }
+    };
+
+    std::string getUuid();
+
+    void openScope();
+
+    void closeScope();
+
+    std::string lookTopId(const std::string& id);
+
+    Scope* topScope;
+
+    int getOperatorPrecedence(BinOpNode* binOp);
+
     std::string generateConstNumber(ConstNumberNode* node);
 
     std::string generateConstBool(ConstBoolNode* node);
 
     std::string generateId(IdentifierNode* node);
 
+    std::string generateFuncCall(FuncCallNode* node);
+
     std::string generateDeclVar(DeclVarNode* node);
 
-    std::string generateMathExpr(BinOpNode* node);
+    std::string generateDeclFunc(DeclFuncNode* node);
 
-    std::string generateBinaryExpr(BinOpNode* node);
+    std::string generateBreakStmt();
+
+    std::string generateIfStmt(IfStmtNode* node);
+
+    std::string generateForLoop(ForLoopNode* node);
+
+    std::string generateBlockStmt(BlockStmtNode* node);
+
+    std::string generateReturnStmt(ReturnStmtNode* node);
+
+    std::string generateBinaryExprMath(BinOpNode* node);
+
+    std::string generateBinaryExprAssign(BinOpNode* node);
+
+    std::string generateGetValue(ASTNode* node);
 
     std::string generateStatement(ASTNode* node);
+
+    int tabCount;
+
+    bool blockScope;
 public:
     std::string generate(ProgramTranslationNode* root);
+
+    BashGenerator(): topScope(new Scope(nullptr)), tabCount(0), blockScope(false) {};
+
+    ~BashGenerator() {
+        delete topScope;
+    }
 };
 
 
