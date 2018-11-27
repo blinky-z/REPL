5b8c68e4d0b00a667bdae418a3068eaa9df5bc2c
TwITe <dm.blink19@gmail.com>
2018-11-28 00:41:38 +0300

    Remove EvalError structure since all checks is performing in Semantic Analyzer

--- EvalResult.h
+++ EvalResult.h
@@ -5,39 +5,6 @@
 #include <map>
 #include <vector>
 
-class EvalError {
-private:
-    std::string message;
-public:
-    enum Error {
-        null,
-        INCOMPATIBLE_OPERAND_TYPES,
-        INVALID_AST,
-        UNDECLARED_VAR,
-        INVALID_LVALUE,
-        UNINITIALIZED_VAR,
-        INVALID_OPERATION,
-        INVALID_VALUE_TYPE,
-        VAR_REDEFINITION,
-        FUNC_REDEFINITION,
-        FUNC_DEFINITION_IS_NOT_ALLOWED,
-        UNDECLARED_FUNC,
-        NO_MATCHING_FUNC,
-        INVALID_RETURN,
-        BREAK_EVAL
-    };
-
-    const std::string& what() const;
-
-    Error errorCode;
-
-    EvalError(Error errCode, std::string errMessage = "");
-
-    EvalError() {
-        errorCode = Error::null;
-    };
-};
-
 struct EvalResult {
 private:
     bool resultBool;
@@ -50,10 +17,6 @@ private:
 
     std::vector<EvalResult> resultBlock;
 public:
-    EvalError error;
-
-    bool isError() const;
-
     ValueType::Type getResultType() const;
 
     double getResultDouble() const;
